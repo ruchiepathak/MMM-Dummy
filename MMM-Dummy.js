c592e482297644f51ad5f3cfd605c6250dddd9aa
Ruchi Pathak <pathakk.ruchi94@gmail.com>
2023-05-09 11:15:43 -0400

    dummy code

---
+++ MMM-Dummy.js
@@ -0,0 +1,32 @@
+Module.register("MMM-Dummy", {
+  // Default module config.
+  defaults: {
+    text: "Hello World!"
+  },
+
+  // Override dom generator.
+  getDom: function() {
+    var wrapper = document.createElement("div");
+    wrapper.innerHTML = this.config.text;
+    return wrapper;
+  },
+
+  getStyles: function() {
+    return ["MMM-Dummy.css"];
+  },
+
+  // Override notification handler.
+  notificationReceived: function(notification, payload, sender) {
+    if (notification === "MY_NOTIFICATION") {
+      // Do something when notification is received.
+    }
+  },
+
+  // Override socket notification handler.
+  socketNotificationReceived: function(notification, payload) {
+    if (notification === "MY_SOCKET_NOTIFICATION") {
+      // Do something when socket notification is received.
+    }
+  }
+});
+
